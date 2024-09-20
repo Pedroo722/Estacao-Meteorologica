8fbc8c5fadcca16984a8c859d68cc01bdc28ee90
Pedro Alexandre <132232273+Pedroo722@users.noreply.github.com>
2024-09-20 14:06:02 -0300

    Adição de seletor de estações

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import ParaibaMap from '../components/ParaibaMap';
-import DashBar from '../components/DashBar'; 
+import DashBar from '../components/DashBar';
 import StationDetails from '../components/StationDetails';
+import { Select } from 'antd';
 
 const Home = () => {
   const [selectedStation, setSelectedStation] = useState(null);
@@ -18,22 +19,51 @@ const Home = () => {
   ]);
 
   // Dados de exemplo
-  const stationDetails = {
+  const [stationDetails, setStationDetails] = useState({
     city: "Campina Grande",
     state: "PB",
     creationDate: "01/01/2000",
     code: "A313",
     latitude: "-23.5505",
     longitude: "-46.6333",
+  });
+
+
+  const handleSelectChange = (value) => {
+    const selected = stations.find(station => station.id === value);
+    if (selected) {
+      setStationDetails(prevDetails => ({
+        ...prevDetails,
+        city: selected.name,
+        code: selected.id,
+      }));
+      setSelectedStation(selected);
+    }
   };
 
   return (
     <div className="container" style={{ display: 'flex', height: '100vh', width: '100%' }}>
-        <DashBar />
-        <div className="map-container" style={{ flexGrow: 1, position: 'relative', marginLeft: '310px', marginRight: '5px', backgroundColor: '#0d0d0d' }}>
-          <ParaibaMap selectedStation={selectedStation} setSelectedStation={setSelectedStation} />
-        </div>
+      <DashBar />
+      <div className="map-container" style={{ flexGrow: 1, position: 'relative', marginLeft: '310px', marginRight: '5px', backgroundColor: '#0d0d0d' }}>
+        <ParaibaMap selectedStation={selectedStation} setSelectedStation={setSelectedStation} />
+      </div>
+      <div className="details-section">
+        <Select
+          showSearch
+          style={{ width: 200, marginBottom: '20px' }}
+          placeholder="Digite para pesquisar"
+          optionFilterProp="label"
+          onChange={handleSelectChange}
+          filterSort={(optionA, optionB) =>
+            (optionA?.label ?? '').toLowerCase().localeCompare((optionB?.label ?? '').toLowerCase())
+          }
+          options={stations.map(station => ({
+            value: station.id,
+            label: station.name,
+          }))}
+        />
         <StationDetails details={stationDetails} />
+      </div>
     </div>
   );
 };
