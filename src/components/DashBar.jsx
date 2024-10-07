8873e2185bc70a6be2399defc3e4abf56b240ae5
Pedroo722 <pedrohenriquealexandre7@gmail.com>
2024-10-07 15:25:31 -0300

    feature: Integração dos Icones de StatusBar com o back-end

--- src/components/DashBar.jsx
+++ src/components/DashBar.jsx
@@ -82,10 +82,6 @@ const DashBar = () => {
           <Theme />
         </div>
       )}
-
-      
-
-      
     </aside>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -61,7 +61,7 @@ const Home = () => {
   return (
     <div className="container" style={{ display: 'flex', height: '100vh', width: '100%' }}>
       <DashBar />
-      <StatusBar />
+      <StatusBar selectedStationCode={selectedStation?.id} />
       <div className="map-container" style={{ flexGrow: 1, position: 'relative', marginRight: '5px', backgroundColor: '#042222', borderRadius: '20px' }}>
         <ParaibaMap selectedStation={selectedStation} setSelectedStation={setSelectedStation} />
       </div>
