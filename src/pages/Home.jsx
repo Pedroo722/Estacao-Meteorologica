03f558bf113588ed2c5d158a5563ea0673543a94
claradiass <claragoncalves370@gmail.com>
2024-10-13 14:12:54 -0300

    Mapa

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -60,13 +60,13 @@ const Home = () => {
 
   return (
     <div className="container" style={{ display: 'flex', height: '100vh', width: '100%' }}>
+      <StatusBar selectedStationCode={selectedStation?.id} />
+      
+      {/* <DashBar /> */}
+      {/* <StatusBar />       */}
+      
 
       
-      <DashBar />
-      <StatusBar />      
-      <div className="map-container" style={{ flexGrow: 1, position: 'relative', marginRight: '5px', backgroundColor: '#042222', borderRadius: '20px'}}>
-
-      <StatusBar selectedStationCode={selectedStation?.id} />
       <div className="map-container" style={{ flexGrow: 1, position: 'relative', marginRight: '5px', backgroundColor: '#042222', borderRadius: '20px' }}>
 
         <ParaibaMap selectedStation={selectedStation} setSelectedStation={setSelectedStation} />
