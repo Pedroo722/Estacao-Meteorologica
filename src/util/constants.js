b29a6e6933873ae26d3f221ebb5efbfce34e1f1b
claradiass <claragoncalves370@gmail.com>
2025-01-29 08:58:34 -0300

    feat: dash

--- src/util/constants.js
+++ src/util/constants.js
@@ -1,6 +1,6 @@
 export const ERRO_SERVIDOR = 'Erro na comunicação com o servidor!';
 
-export const baseUrl = 'http://localhost:8080/api/';
+export const baseUrl = 'http://api-estacao-meteorologica.onrender.com/api/';
 
 export const baseUrlStationDetails = baseUrl + 'estacoes/info/';
 export const baseUrlWeatherData = baseUrl + 'estacoes/data/';
